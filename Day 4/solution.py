882e31ef233d808fe6f8b7d54d4f09b954ebb814
Martin Jakobsson <92855097+0xMishee@users.noreply.github.com>
2024-12-06 19:52:02 +0100

    Implement solve_two function to count occurrences of "MAS" diagonally in a grid

--- Day 4/solution.py
+++ Day 4/solution.py
@@ -51,12 +51,46 @@ def solve_one(data: list) -> int:
     return word_count
 
 
-def solve_two(data: str) -> int:
-    return 0
+def solve_two(data: list) -> int:
+    word: str = "MAS"
+    word_count: int = 0
+
+    # Create a grid from the input data
+    grid: list = []
+    for line in enumerate(data):
+        grid.append(list(line[1].strip()))
+
+    rows = len(grid)
+    cols = len(grid[0]) if rows > 0 else 0
+
+
+    for line in range(rows):
+        for index, c in enumerate(grid[line]):
+            if c == "M":
+                try:
+                    # Diagonal
+                    if line + 2 < rows and index + 2 < cols and "".join([grid[line + i][index + i] for i in range(3)]) == word:
+                        word_count += 1
+                        #print("Diagonal", line, index)
+                    if line - 2 >= 0 and index - 2 >= 0 and "".join([grid[line - i][index - i] for i in range(3)]) == word:
+                        word_count += 1
+                    if line + 2 < rows and index - 2 >= 0 and "".join([grid[line + i][index - i] for i in range(3)]) == word:
+                        word_count += 1
+                    if line - 2 >= 0 and index + 2 < cols and "".join([grid[line - i][index + i] for i in range(3)]) == word:
+                        word_count += 1
+                except IndexError:
+                    # Out of bounds catch
+                    pass
+
+
+
+
+    return word_count
 
 
 if __name__ == "__main__":
     with open("input.txt", "r", encoding="utf-8") as f:
         data = f.readlines()
 
-    print(solve_one(data))
+    print(f"Number of XMAS Solve One:", solve_one(data))
+    print(f"Number of Cross MAS:",solve_two(data))
