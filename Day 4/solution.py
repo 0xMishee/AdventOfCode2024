2b3115bc3c8510b371b6a5a9faceb93dc636715f
Martin Jakobsson <92855097+0xMishee@users.noreply.github.com>
2024-12-06 08:59:28 +0100

    Refactor comments in Day 4 solution for clarity and consistency

--- Day 4/solution.py
+++ Day 4/solution.py
@@ -18,35 +18,29 @@ def solve_one(data: list) -> int:
         for index, c in enumerate(grid[line]):
             if c == "X":
                 try:
-                    # Horizontal check
+                    # Horizontal 
                     if index + 3 < cols and grid[line][index:index + 4] == list(word):
                         word_count += 1
 
-                    # Reverse Horizontal check
+                    # Reverse Horizontal 
                     if index - 3 >= 0 and grid[line][index - 3:index + 1] == list(word[::-1]):
                         word_count += 1
 
-                    # Vertical check
+                    # Vertical 
                     if line + 3 < rows and "".join([grid[line + i][index] for i in range(4)]) == word:
                         word_count += 1
 
-                    # Reverse Vertical check
+                    # Reverse Vertical 
                     if line - 3 >= 0 and "".join([grid[line - i][index] for i in range(4)]) == word:
                         word_count += 1
 
-                    # Diagonal (down-right)
+                    # Diagonal
                     if line + 3 < rows and index + 3 < cols and "".join([grid[line + i][index + i] for i in range(4)]) == word:
                         word_count += 1
-
-                    # Diagonal (up-left)
                     if line - 3 >= 0 and index - 3 >= 0 and "".join([grid[line - i][index - i] for i in range(4)]) == word:
                         word_count += 1
-
-                    # Diagonal (down-left)
                     if line + 3 < rows and index - 3 >= 0 and "".join([grid[line + i][index - i] for i in range(4)]) == word:
                         word_count += 1
-
-                    # Diagonal (up-right)
                     if line - 3 >= 0 and index + 3 < cols and "".join([grid[line - i][index + i] for i in range(4)]) == word:
                         word_count += 1
 
